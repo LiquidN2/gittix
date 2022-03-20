4038d9353a1acf45baab77a865695d470dc60683
Hugh Nguyen <hhnguyen255@gmail.com>
2022-03-20 21:21:28 +0700

    setup routes for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 
+import { currentUserRouter } from './routes/current-user';
+import { signupRouter } from './routes/signup';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/api/users/current-user', (req, res) => {
-  res.send('Hello World');
-});
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
