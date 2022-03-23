d574b85514269c96125f6ae543c59ace6498f6c0
Hugh Nguyen <hhnguyen255@gmail.com>
2022-03-23 13:09:23 +0700

    generate JWT and send as cookie

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+import 'express-async-errors';
 
 import { currentUserRouter } from './routes/current-user';
 import { signupRouter } from './routes/signup';
@@ -11,23 +12,31 @@ import { NotFoundError } from './errors/not-found-error';
 import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
+app.set('trust proxy', true);
 
+// GLOBAL MIDDLEWARES
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false, // encryption to be handled by JWT
+    secure: true, // send cookie via HTTPS only
+  })
+);
 
-// Route Handlers
+// ROUTE HANDLERS
 app.use(currentUserRouter);
 app.use(signupRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
-
 app.all('*', async () => {
   throw new NotFoundError();
 });
 
-// Error Handlers
+// ERROR HANDLERS
 app.use(errorHandler);
 
+// APP INITIALIZATION
 const init = async () => {
   // Connect to DB
   try {
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import * as jose from 'jose';
+import { createSecretKey } from 'crypto';
 
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
 import { User } from '../models/user';
+
+const JWT_SECRET = process.env.JWT_SECRET || 'jwt_secret';
+const JWT_ISSUER = process.env.JWT_ISSUER || 'gittix:auth-srv:issuer';
+const JWT_AUDIENCE = process.env.JWT_AUDIENCE || 'gittix:auth-srv:audience';
+const JWT_EXPIRATION_TIME = process.env.JWT_EXPIRATION_TIME || '1d';
 
 const router = express.Router();
 
@@ -33,6 +40,21 @@ router.post(
     // Password hashing is done by mongoose middleware before saving the doc
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    // Create a secret key of type KeyObject from a jwt secret
+    const privateKey = createSecretKey(JWT_SECRET, 'utf-8');
+    // Sign the key
+    const userJwt = await new jose.SignJWT({ id: user.id, email: user.email })
+      .setProtectedHeader({ alg: 'HS256' })
+      .setIssuedAt()
+      .setIssuer(JWT_ISSUER)
+      .setAudience(JWT_AUDIENCE)
+      .setExpirationTime(JWT_EXPIRATION_TIME)
+      .sign(privateKey);
+
+    // Store JWT on session object
+    req.session = { jwt: userJwt };
 
     res.status(201).send(user);
   }
