efa45262f55bf8f883217ddaf65ab878ae23b9b8
Hugh Nguyen <hhnguyen255@gmail.com>
2022-04-06 18:28:41 +0700

    completed handler for creating ticket

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,16 +1,64 @@
-import { Router, Request, Response } from 'express';
+import { Request, Response, Router } from 'express';
+import { Types } from 'mongoose';
 import { body } from 'express-validator';
-import { authenticate, validateRequest } from '@hngittix/common';
+import {
+  authenticate,
+  BadRequestError,
+  NotFoundError,
+  UnauthorizedRequestError,
+  OrderStatus,
+  validateRequest,
+} from '@hngittix/common';
 
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60; // 15 minutes
 const router = Router();
+const { ObjectId } = Types;
 
 router.post(
   '/api/orders',
   authenticate,
-  body('ticketId').trim().notEmpty().withMessage('Ticket id must be valid'),
+  body('ticketId')
+    .trim()
+    .notEmpty()
+    .custom((input: string) => ObjectId.isValid(input))
+    .withMessage('Ticket id must be valid'),
   validateRequest,
   async (req: Request, res: Response) => {
-    res.status(201).send({});
+    const { ticketId } = req.body;
+
+    // Check if ticket exists
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) throw new NotFoundError('Ticket not found');
+
+    // Check if ticket is reserved (ticket is associated with an order with status not 'cancelled')
+    const isReserved = await ticket.isReserved();
+    if (isReserved) throw new BadRequestError('Ticket is reserved');
+
+    // Calculate expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // Create the order and save to db
+    if (typeof req.currentUser?.id !== 'string') {
+      throw new UnauthorizedRequestError();
+    }
+    const userId = req.currentUser.id;
+
+    const order = Order.build({
+      ticket,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      userId: new ObjectId(userId),
+    });
+
+    await order.save();
+
+    // Publish event
+
+    res.status(201).send(order);
   }
 );
 
