095c80bed6ffe955fc9a84ed6ba645e802e35bd4
Hugh Nguyen <hhnguyen255@gmail.com>
2022-04-06 12:20:51 +0700

    updated cookie session name

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,6 +3,7 @@ import cookieSession from 'cookie-session';
 import 'express-async-errors';
 
 import { NotFoundError, errorHandler } from '@hngittix/common';
+import { createOrderRouter } from './routes/new';
 
 const app = express();
 
@@ -12,13 +13,20 @@ app.set('trust proxy', true);
 // GLOBAL MIDDLEWARES
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+if (!process.env.COOKIE_SESSION_NAME)
+  throw new Error('COOKIE_SESSION_NAME must be defined');
 app.use(
   cookieSession({
-    name: 'gittix-session',
+    name: process.env.COOKIE_SESSION_NAME,
     signed: false, // encryption to be handled by JWT
     secure: process.env.NODE_ENV !== 'test', // send cookie via HTTPS only
   })
 );
+
+// -----------------------
+// ROUTE HANDLERS
+app.use(createOrderRouter);
 
 // -----------------------
 // ERROR HANDLERS
