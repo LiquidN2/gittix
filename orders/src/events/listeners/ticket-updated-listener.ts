ad92df1a3f4359ea3a5fef66ae71379326908d9a
Hugh Nguyen <hhnguyen255@gmail.com>
2022-04-19 13:29:33 +0700

    change the order status to awaiting payment once the ticket is reserved

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -1,20 +1,43 @@
 import { Message } from 'node-nats-streaming';
-import { Listener, Subjects, TicketUpdatedEvent } from '@hngittix/common';
+import {
+  Listener,
+  Subjects,
+  TicketUpdatedEvent,
+  OrderStatus,
+} from '@hngittix/common';
+
 import { QueueGroupName } from './queue-group-name';
 import { Ticket } from '../../models/ticket';
+import { Order } from '../../models/order';
 
 export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
   queueGroupName = QueueGroupName.OrdersService;
 
   async onMessage(eventData: TicketUpdatedEvent['data'], msg: Message) {
-    const { title, price } = eventData;
+    const { title, price, orderId } = eventData;
 
+    // Find the ticket
     const ticket = await Ticket.findByEventData(eventData);
     if (!ticket) throw new Error('Ticket not found');
 
+    // Update the ticket
     ticket.set({ title, price });
     await ticket.save();
+
+    // If the ticket event data has orderId (ticket is reserved), change order status to awaiting payment
+    if (orderId) {
+      const order = await Order.findById(orderId);
+
+      if (!order) throw new Error('Order not found');
+      if (order.status === OrderStatus.Cancelled)
+        throw new Error('Order was cancelled');
+      if (order.status === OrderStatus.Complete)
+        throw new Error('Order already paid for');
+
+      order.status = OrderStatus.AwaitingPayment;
+      await order.save();
+    }
 
     msg.ack();
   }
