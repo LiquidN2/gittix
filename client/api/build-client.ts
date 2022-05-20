a6a397128475b1066b8f11fafdb9609be7ba0c78
Hugh Nguyen <hhnguyen255@gmail.com>
2022-05-20 15:36:54 +0700

    updated build client fn

--- client/api/build-client.ts
+++ client/api/build-client.ts
@@ -1,14 +1,54 @@
 import axios, { AxiosInstance, AxiosRequestHeaders } from 'axios';
-import { AppContext } from 'next/app';
 
 // appContext.ctx.req only available on the server
 // if on the server build a custom axios client
 // else use a regular axios client for browser
-export const buildClient = (appContext: AppContext): AxiosInstance =>
-  appContext.ctx.req
-    ? axios.create({
-        baseURL:
-          'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-        headers: appContext.ctx.req.headers as AxiosRequestHeaders,
-      })
-    : axios.create({ baseURL: '/' });
+// export const buildClient = (
+//   context: AppContext | GetServerSidePropsContext
+// ): AxiosInstance => {
+//   if ((context as AppContext).ctx.req) {
+//     return axios.create({
+//       baseURL:
+//         'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+//       headers: (context as AppContext).ctx.req?.headers as AxiosRequestHeaders,
+//     });
+//   }
+
+//   return axios.create({ baseURL: '/' });
+
+//   // return context.ctx.req
+//   //   ? axios.create({
+//   //       baseURL:
+//   //         'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+//   //       headers: context.ctx.req.headers as AxiosRequestHeaders,
+//   //     })
+//   //   : axios.create({ baseURL: '/' });
+// };
+
+interface Context {
+  ctx?: any;
+  req?: any;
+}
+
+export function buildClient<T extends Context>(context: T): AxiosInstance {
+  // Check if the context param is of type AppContext
+  if (context.ctx?.req) {
+    return axios.create({
+      baseURL:
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: context.ctx.req.headers as AxiosRequestHeaders,
+    });
+  }
+
+  // Check if the context param is of type GetServerSidePropsContext or GetStaticPropsContent
+  if (context.req) {
+    return axios.create({
+      baseURL:
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: context.req.headers as AxiosRequestHeaders,
+    });
+  }
+
+  // if no context returns a simple axios instance for browser request
+  return axios.create({ baseURL: '/' });
+}
