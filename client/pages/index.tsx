e3472fba3baed5906e3723a87553e120638a02ad
Hugh Nguyen <hhnguyen255@gmail.com>
2022-05-20 15:42:53 +0700

    minor updates

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,19 @@
-import type { NextPage } from 'next';
+import type { NextPage, GetServerSideProps } from 'next';
 import Head from 'next/head';
+import { useEffect } from 'react';
+
+import { buildClient } from '../api/build-client';
 
 interface HomePageProps {
   currentUser: Record<string, any> | null;
+  tickets?: any;
 }
 
-const Home: NextPage<HomePageProps> = ({ currentUser }) => {
+const Home: NextPage<HomePageProps> = ({ currentUser, tickets }) => {
+  useEffect(() => {
+    console.log(tickets);
+  }, []);
+
   return (
     <div>
       <Head>
@@ -16,8 +24,22 @@ const Home: NextPage<HomePageProps> = ({ currentUser }) => {
       </Head>
       <h1>Landing page !</h1>
       <p>{currentUser ? 'Logged in' : 'Not logged in'}</p>
+      {tickets.length > 0 &&
+        tickets.map((ticket: any) => <div key={ticket.id}>{ticket.title}</div>)}
     </div>
   );
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const client = buildClient(context);
+
+  const { data } = await client.get('/api/tickets');
+
+  return {
+    props: {
+      tickets: data.tickets,
+    },
+  };
+};
